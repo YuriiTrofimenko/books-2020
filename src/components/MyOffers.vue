61656a35bcd508b2e22a1393801a07cd203fea2c
edvigindi <edvigindi@gmail.com>
2020-01-18 13:36:50 +0200

    cards-row

--- src/components/MyOffers.vue
+++ src/components/MyOffers.vue
@@ -16,8 +16,24 @@
           span
             vs-icon(icon='add' size='medium' color='white' bg='rgb(70, 150, 0)' @click='activeAddBookPrompt = true')
     vs-row
-      vs-col(:key='index' v-for='book,index in books' vs-type='flex' vs-justify='center' vs-align='center' vs-w='6')
-        span {{book.title}}
+      vs-col(:key='index' v-for='book,index in books' vs-type='flex' vs-justify='center' vs-align='center' vs-w='3')
+        template
+          vs-row(vs-justify='center')
+            vs-col(type='flex' vs-justify='center' vs-align='center' vs-w='12')
+              vs-card.cardx
+                div(slot='header')
+                  h3
+                    | {{book.title}}
+                div(slot='media')
+                  img(src='../assets/logo.png')
+                div
+                  span
+                    | Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                div(slot='footer')
+                  vs-row(vs-justify='flex-end')
+                    vs-button(type='gradient' color='danger' icon='favorite')
+                    vs-button(color='primary' icon='turned_in_not')
+                    vs-button(color='rgb(230,230,230)' color-text='rgb(50,50,50)' icon='settings')
 </template>
 
 <script>
