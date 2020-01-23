1f6f4b205a276dac813332131b84e5cb1bc315e3
edvigindi <edvigindi@gmail.com>
2020-01-23 19:02:58 +0200

    firebase

--- src/helpers/firebaseConfig.js
+++ src/helpers/firebaseConfig.js
@@ -1,10 +1,10 @@
 export const firebaseConfig = {
-  apiKey: 'AIzaSyCudgz09YeuGMhPPXfE9x4Oj901lAZgcSc',
-  authDomain: 'books-as-a-gift.firebaseapp.com',
-  databaseURL: 'https://books-as-a-gift.firebaseio.com',
-  projectId: 'books-as-a-gift',
-  storageBucket: 'books-as-a-gift.appspot.com',
-  messagingSenderId: '615856961226',
-  appId: '1:615856961226:web:9b14b7369256c9967aa633',
-  measurementId: 'G-ZC5Q0VKNYJ'
+  apiKey: 'AIzaSyDf6uZBidOCa_oL-9SfLuHJssDYHAyww_0',
+  authDomain: 'books-as-a-gift-2.firebaseapp.com',
+  databaseURL: 'https://books-as-a-gift-2.firebaseio.com',
+  projectId: 'books-as-a-gift-2',
+  storageBucket: 'books-as-a-gift-2.appspot.com',
+  messagingSenderId: '497611132330',
+  appId: '1:497611132330:web:aeb54e228c0d1867226fd3',
+  measurementId: 'G-82KWWPW5S5'
 }
