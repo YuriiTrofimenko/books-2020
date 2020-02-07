0eda7d380a2bbe01020a59f9d376f0026187914c
YuriiTrofimenko <tyaa@ukr.net>
2020-02-07 18:08:43 +0200

    search infscroll step1

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,22 +1,94 @@
 <template lang="pug">
-  .search Search
+  div
+    vs-row.infinite-wrapper
+      vs-col(:key='index' v-for='book,index in books' vs-type='flex' vs-justify='center' vs-align='center' vs-w='3')
+        template
+          vs-row(vs-justify='center')
+            vs-col(type='flex' vs-justify='center' vs-align='center' vs-w='12')
+              vs-card.cardx
+                div(slot='header')
+                  h3
+                    | {{book.title}}
+                div(slot='media')
+                  img(v-if='book.image' :src='book.image')
+                  img(v-if='!book.image' src='../assets/logo.png')
+                div
+                  span
+                    | {{book.description}}
+                div(slot='footer')
+                  vs-row(vs-justify='flex-end')
+                    vs-button(type='gradient' color='danger' icon='favorite')
+                    vs-button(color='primary' icon='turned_in_not')
+                    vs-button(color='rgb(230,230,230)' color-text='rgb(50,50,50)' icon='settings')
+      infinite-loading(@infinite='booksInfiniteHandler' force-use-infinite-wrapper='.infinite-wrapper')
 </template>
 
 <script>
+import InfiniteLoading from 'vue-infinite-loading'
 export default {
   name: 'Search',
+  components: { InfiniteLoading },
   data () {
     return {
-      msg: 'Welcome to Your Vue.js App'
+      isBooksListChanged: false
     }
   },
   computed: {
-    cmp1 () {
-      return 'Test'
+    books () {
+      // источник данных о книгах
+      return this.$store.getters.books
+    }
+  },
+  watch: {
+    // Если изменился список
+    books (newVal, oldVal) {
+      this.isBooksListChanged = true
+    }
+  },
+  created () {
+    this.$store.dispatch('clearBooks')
+      .then(() => {
+        if (this.books.length !== 0) {
+          this.books.length = 0
+        }
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  },
+  methods: {
+    booksInfiniteHandler ($state) {
+      this.$store.dispatch('loadBooks')
+        .then(() => {
+          // console.log('isBooksListChanged', this.isBooksListChanged)
+          if (this.isBooksListChanged) {
+            console.log('$state.loaded()')
+            $state.loaded()
+          } else {
+            // $state.complete()
+            if (this.$store.getters.currentBooksOwner === -1) {
+              console.log('$state.complete()')
+              $state.complete()
+            } else {
+              console.log('$state.loaded()2')
+              $state.loaded()
+            }
+          }
+          this.isBooksListChanged = false
+        })
+        .catch(err => {
+          console.log(err)
+        })
     }
   }
 }
 </script>
 
 <style scoped lang="stylus">
+  img
+    max-height 300px
+    max-width 100%
+  .infinite-wrapper
+    overflow scroll
+    height 800px
 </style>
--- src/store/book.js
+++ src/store/book.js
@@ -7,7 +7,10 @@ export default ({
     // Локальный массив Books
     books: [],
     myBooks: [],
-    oldestMyBookKeyRef: null
+    oldestMyBookKeyRef: null,
+    oldestBookKeyRef: null,
+    currentBooksOwner: null,
+    ownersBooksRemain: false
   },
   mutations: {
     newBook (
@@ -61,6 +64,15 @@ export default ({
     },
     setOldestMyBookKeyRef (state, payload) {
       state.oldestMyBookKeyRef = payload
+    },
+    setOldestBookKeyRef (state, payload) {
+      state.oldestBookKeyRef = payload
+    },
+    setCurrentBooksOwner (state, payload) {
+      state.currentBooksOwner = payload
+    },
+    setOwnersBooksRemain (state, payload) {
+      state.ownersBooksRemain = payload
     }
   },
   actions: {
@@ -92,6 +104,13 @@ export default ({
             .once('value')
         let booksCount = booksCountResponse.val()
         if (!booksCount) {
+          // Добавляем в корень удаленного хранилища, в дочерний узел usersIds
+          // Id данного пользователя
+          firebase.database()
+            .ref('usersIds')
+            .push({'id': getters.user.id})
+          // Добавляем в узел пользователя в удаленном хранилище, в дочерний узел booksCount
+          // объект счетчика собственных книг с начальным значением 1
           firebase.database()
             .ref(getters.user.id + '/booksCount')
             .push({'count': 1})
@@ -99,7 +118,7 @@ export default ({
           let arrayOfKeys =
             Object.keys(booksCount)
           booksCount = booksCount[arrayOfKeys[0]]
-          console.log(booksCount)
+          // console.log(booksCount)
           firebase.database()
             .ref(getters.user.id + '/booksCount/' + arrayOfKeys[0])
             // .orderByKey()
@@ -119,12 +138,15 @@ export default ({
         throw error
       }
     },
+    // Загрузка собственных книг пользователя
     async loadMyBooks ({commit, getters}) {
       commit('clearError')
       commit('setLoading', true)
       try {
-        // ***
+        // Если нет ссылки на последнюю загруженную собственную книгу
         if (!getters.oldestMyBookKeyRef) {
+          // Пытаемся получить из удаленного хранилища четыре собственные книги пользователя,
+          // отсортировав по ключам
           const booksResponse =
             await firebase.database()
               .ref(getters.user.id + '/books')
@@ -179,7 +201,7 @@ export default ({
             let arrayOfCountKeys =
               Object.keys(booksCount)
             booksCount = booksCount[arrayOfCountKeys[0]]
-            console.log(booksCount.count, getters.myBooks.length)
+            // console.log(booksCount.count, getters.myBooks.length)
             if (booksCount.count > getters.myBooks.length) {
               const booksResponse =
                 await firebase.database()
@@ -237,49 +259,211 @@ export default ({
         throw error
       }
     },
-    async loadBooks ({commit, getters}) {
+    async loadBooks ({commit, getters, dispatch}) {
       commit('clearError')
       commit('setLoading', true)
       try {
-        const booksResponse =
-          await firebase.database()
-            .ref(getters.user.id + '/books')
-            // .orderByChild('user')
-            // .equalTo(getters.user.id)
-            .once('value')
-        // Get value
-        const books = booksResponse.val()
-        // console.log(books)
-        if (books) {
-          // New array
-          const booksArray = []
-          // Get task key (id)
-          Object.keys(books).forEach(key => {
-            const b = books[key]
-            // console.log(n)
-            booksArray.push(
-              new Book(
-                b.title,
-                b.author,
-                b.description,
-                b.country,
-                b.city,
-                b.type,
-                b.image,
-                b.active,
-                // n.user,
-                key
-              )
-            )
-          })
-          const payload = {
-            target: 'books',
-            books: booksArray
-          }
-          // Send mutation
-          commit('loadBooks', payload)
+        console.log('test')
+        console.log('oldestBookKeyRef', getters.oldestBookKeyRef)
+        console.log('currentBooksOwner', getters.currentBooksOwner)
+        console.log('ownersBooksRemain', getters.ownersBooksRemain)
+        console.log('end test')
+        // Если не осталось ИД пользователей в списке
+        if (getters.currentBooksOwner === -1) {
+          console.log('-------------------1')
+          // Завершаем работу данного действия - книг для загрузки в удаленном хранилище не осталось
+          commit('setCurrentBooksOwner', null)
+          commit('setLoading', false)
+          return
         }
-
+        // Если у текущего владельца остаются книги -
+        // не меняем владельца
+        if (getters.ownersBooksRemain) {
+          console.log('ownersBooksRemain', getters.ownersBooksRemain)
+        } else {
+          // Иначе если текущий владелец книг установлен
+          if (getters.currentBooksOwner) {
+            // Пытаемся установить следующего владельца,
+            // запрашивая из удаленного хранилища ИД текущего владельца книг и следующего за ним
+            console.log('currentBooksOwner', getters.currentBooksOwner)
+            const userIdsResponse =
+              await firebase.database()
+                .ref('usersIds')
+                .orderByKey()
+                .endAt(getters.currentBooksOwner.key)
+                .limitToLast(2)
+                .once('value')
+            // Извлекаем ответ из объекта-оболочки результата запроса
+            const userIds = userIdsResponse.val()
+            console.log('userIds', userIds)
+            // Если объект получен
+            if (userIds) {
+              // Оставляем только ключ нового владельца из двух полученных
+              let arrayOfKeys =
+                Object.keys(userIds)
+                  .sort()
+                  .reverse()
+                  .slice(1)
+              // Когда не останется ни одного ИД в списке ИД пользователей -
+              // сохраняем в локальное хранилище значение -1
+              console.log('arrayOfKeys', arrayOfKeys)
+              commit('setCurrentBooksOwner', userIds[arrayOfKeys[0]] ? {key: arrayOfKeys[0], id: userIds[arrayOfKeys[0]].id} : -1)
+              if (userIds[arrayOfKeys[0]]) {
+                commit('setOwnersBooksRemain', true)
+                commit('setOldestBookKeyRef', null)
+                console.log('rebuilt', getters.ownersBooksRemain)
+              }
+            }
+          } else {
+            // Иначе сортируем в удаленном хранилище Id всех пользователей и находим Id самого первого
+            const userIdsResponse =
+              await firebase.database()
+                .ref('usersIds')
+                .orderByKey()
+                .limitToLast(1)
+                .once('value')
+            // Извлекаем значение из оболочки ответа удаленного хранилища
+            const userIds = userIdsResponse.val()
+            if (userIds) {
+              let arrayOfKeys = Object.keys(userIds)
+              commit('setCurrentBooksOwner', userIds[arrayOfKeys[0]] ? {key: arrayOfKeys[0], id: userIds[arrayOfKeys[0]].id} : -1)
+            }
+          }
+        }
+        // Если нет ссылки на последнюю загруженную книгу
+        if (!getters.oldestBookKeyRef) {
+          // Пытаемся получить из удаленного хранилища четыре книги текущего пользователя,
+          // отсортировав по ключам
+          const booksResponse =
+            await firebase.database()
+              .ref(getters.currentBooksOwner.id + '/books')
+              .orderByKey()
+              .limitToLast(4)
+              .once('value')
+          // Get value
+          const books = booksResponse.val()
+          console.log('books1', books)
+          if (books) {
+            let arrayOfKeys =
+              Object.keys(books)
+                .sort()
+                .reverse()
+            commit('setOldestBookKeyRef', arrayOfKeys[arrayOfKeys.length - 1])
+            // ***
+            // New array
+            const booksArray = []
+            // Get task key (id)
+            arrayOfKeys.forEach(key => {
+              const b = books[key]
+              // console.log(n)
+              booksArray.push(
+                new Book(
+                  b.title,
+                  b.author,
+                  b.description,
+                  b.country,
+                  b.city,
+                  b.type,
+                  b.image,
+                  b.active,
+                  // n.user,
+                  key
+                )
+              )
+            })
+            const payload = {
+              target: 'books',
+              books: booksArray
+            }
+            // Send mutation
+            commit('loadBooks', payload)
+            if (booksArray.length > 0) {
+              commit('setOwnersBooksRemain', true)
+            }
+          }
+        } else {
+          /* // Попытка получить из удаленного хранилища объект
+          // счетчика книг данного пользователя
+          const booksCountResponse =
+            await firebase.database()
+              .ref(getters.currentBooksOwner + '/booksCount')
+              .once('value')
+          let booksCount = booksCountResponse.val()
+          // Если получен объект со значением счетчика
+          if (booksCount) {
+            // Извлекаем значение счетчика из единственного объекта (под индексом 0)
+            let arrayOfCountKeys =
+              Object.keys(booksCount)
+            booksCount = booksCount[arrayOfCountKeys[0]]
+            // Если количество книг в локальном хранилище меньше, чем в удаленном
+            console.log(booksCount.count, ' - ', getters.books.length)
+            if (booksCount.count > getters.books.length) { */
+          // Пытаемся получить следующие 4 + 1 последнюю из предыдущего множества
+          const booksResponse =
+            await firebase.database()
+              .ref(getters.currentBooksOwner.id + '/books')
+              .orderByKey()
+              .endAt(getters.oldestBookKeyRef)
+              .limitToLast(5)
+              .once('value')
+          // Get value
+          const books = booksResponse.val()
+          console.log('books', books)
+          if (books) {
+            // Сортируем полученное множество книг по ключам и удаляем из него
+            // одну последнюю из предыдущего множества, которая была нужна только для
+            // определения начала очередного множества
+            let arrayOfKeys =
+              Object.keys(books)
+                .sort()
+                .reverse()
+                .slice(1)
+            if (arrayOfKeys.length > 0) {
+              // Последнюю книгу из текущего множества сохраняем в локальном хранилище
+              // для опредеоения начала следующего множества
+              commit('setOldestBookKeyRef', arrayOfKeys[arrayOfKeys.length - 1])
+              // New array
+              const booksArray = []
+              // Get task key (id)
+              arrayOfKeys.forEach(key => {
+                const b = books[key]
+                // console.log(n)
+                booksArray.push(
+                  new Book(
+                    b.title,
+                    b.author,
+                    b.description,
+                    b.country,
+                    b.city,
+                    b.type,
+                    b.image,
+                    b.active,
+                    // n.user,
+                    key
+                  )
+                )
+              })
+              const payload = {
+                target: 'books',
+                books: booksArray
+              }
+              // Send mutation
+              commit('setOwnersBooksRemain', true)
+              commit('loadBooks', payload)
+            } else {
+              console.log('disaster!')
+              commit('setOwnersBooksRemain', false)
+              // dispatch('loadBooks')
+            }
+          }
+          // commit('setOwnersBooksRemain', true)
+          /* } else {
+            commit('setOwnersBooksRemain', false)
+            dispatch('loadBooks')
+          } */
+        }
+        // }
+        // ***
         commit('setLoading', false)
       } catch (error) {
         commit('setLoading', false)
@@ -317,6 +501,11 @@ export default ({
         commit('setError', error.message)
         throw error
       }
+    },
+    async clearBooks ({commit, getters}) {
+      commit('setOldestBookKeyRef', null)
+      commit('setCurrentBooksOwner', null)
+      commit('setOwnersBooksRemain', false)
     }
   },
   getters: {
@@ -328,6 +517,15 @@ export default ({
     },
     oldestMyBookKeyRef (state) {
       return state.oldestMyBookKeyRef
+    },
+    oldestBookKeyRef (state) {
+      return state.oldestBookKeyRef
+    },
+    currentBooksOwner (state) {
+      return state.currentBooksOwner
+    },
+    ownersBooksRemain (state) {
+      return state.ownersBooksRemain
     }
   }
 })
