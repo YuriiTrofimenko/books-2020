62d00f899209090a04dcba758567660d2f705034
YuriiTrofimenko <tyaa@ukr.net>
2020-02-12 13:17:04 +0200

    extend the book model

--- src/store/BookModel.js
+++ src/store/BookModel.js
@@ -1,14 +1,15 @@
 export default class BookModel {
-  constructor (title, author, description, country, city, type, image, active, id = null) {
+  constructor (title, author, genre, description, country, city, type, image, active, updatedAt, id = null) {
     this.title = title
     this.author = author
+    this.genre = genre
     this.description = description
     this.country = country
     this.city = city
     this.type = type
     this.image = image
     this.active = active
-    // his.genre = active
+    this.updatedAt = updatedAt 
     this.id = id
   }
 }
